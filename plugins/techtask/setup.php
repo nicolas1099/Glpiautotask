d123eece44c6b70442b934d2ef28b4d559d3829b
Nicolás Rivas Riascos <nrivas@DCATAL.LOCAL>
2026-06-22 16:35:02 +0200

    instalador version 11 GLPI

--- plugins/techtask/setup.php
+++ plugins/techtask/setup.php
@@ -1,5 +1,20 @@
 <?php
 
+// Autocarga de clases PSR-4 para el plugin
+spl_autoload_register(function ($class) {
+    $prefix = 'GlpiPlugin\\Techtask\\';
+    $base_dir = __DIR__ . '/src/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
 // setup.php
 define('PLUGIN_TECHTASK_VERSION', '1.0.0');
 
